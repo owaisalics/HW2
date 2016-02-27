4821b16f855655bce732e0ee5910744909e277e9
owaisalics1 <17100271@lums.edu.pk>
2016-02-27 22:56:33 +0000

    1-sorting

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,8 +11,9 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @movies = Movie.order(params[:sort_param])
   end
+  
 
   def new
     # default: render 'new' template
